a1ba7b89a1e83ecca118158e8117b0fa8e993c39
Max Payson <mpayson@esri.com>
2018-06-25 10:34:26 -0700

    removed log

--- safegraph/utils.js
+++ safegraph/utils.js
@@ -65,8 +65,6 @@ const utils = {
     }
   },
   createCatDataset: (poi, field, alias) => {
-    console.log("HERE")
-    console.log(poi);
     const where = utils.getPoiExp(poi);
     const visitSumField = "sum";
     return {
